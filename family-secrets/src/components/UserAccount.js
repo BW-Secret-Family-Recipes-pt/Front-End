b7be441171be31145b7051419d6c801b44bb6343
Liz Shulman <corgipower@hotmail.com>
2020-09-17 19:36:31 -0400

    basic form and RUD set up

--- family-secrets/src/components/UserAccount.js
+++ family-secrets/src/components/UserAccount.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const UserAccount = () => {
     /* CRUD -
@@ -6,10 +7,68 @@ const UserAccount = () => {
     read will be here, in the form of showing the user his account details
     update will be here in the form of allowing the user to edit his account details
     delete will be here in the form of allowing the user to delete his account */
+
+    const [user, setUser] = useState({
+        email: '',
+        first_name: '',
+        last_name: '',
+        id: '',
+    });
+
+    useEffect(() => {
+        Axios
+            .get('https://reqres.in/api/users/2')
+            .then(res => {
+                setUser(res.data.data);
+            })
+            .catch(err => console.log(err));
+    }, []);
+
+    const changeHandler = e => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const submitHandler = e => {
+        e.preventDefault();
+        Axios
+            .put(`https://reqres.in/api/users/${user.id}`, user)
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
+    }
+
+    const deleteHandler = () => {
+        Axios
+        .delete(`https://reqres.in/api/users/${user.id}`, user)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
+
     return(
-        <div>
-            <p>user account</p>
-        </div>
+        <>
+            <form onSubmit={submitHandler}>
+                <label htmlFor='first_name'>
+                    First Name: 
+                    <input name='first_name' type='text' value={user.first_name} onChange={changeHandler} />
+                </label>
+                <label htmlFor='last_name'>
+                    Last Name: 
+                    <input name='last_name' type='text' value={user.last_name} onChange={changeHandler} />
+                </label>
+                <label htmlFor='email'>
+                    Email: 
+                    <input name='email' type='email' value={user.email} onChange={changeHandler} />
+                </label>
+
+                <button type='submit'>Submit</button>
+            </form>
+
+            <div onClick={deleteHandler}>
+                <button>Delete Account</button>
+            </div>
+        </>
     )
 }
 
