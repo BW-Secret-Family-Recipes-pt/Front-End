b9c7a236d3985515b657c0328eeea7164cc2320e
Liz Shulman <corgipower@hotmail.com>
2020-09-16 21:48:19 -0400

    created component, added route for it

---
+++ family-secrets/src/components/UserAccount.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UserAccount = () => {
+    /* CRUD -
+    create - will be in the registration form
+    read will be here, in the form of showing the user his account details
+    update will be here in the form of allowing the user to edit his account details
+    delete will be here in the form of allowing the user to delete his account */
+    return(
+        <div>
+            <p>user account</p>
+        </div>
+    )
+}
+
+export default UserAccount;
