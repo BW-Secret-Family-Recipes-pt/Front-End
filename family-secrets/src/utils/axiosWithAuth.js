8e07c5d01096505c66792ee1a9c0912ea0a7a876
Liz Shulman <corgipower@hotmail.com>
2020-09-17 20:45:15 -0400

    edited axiosWithAuth because I don't see how to use it with reqres anyway

--- family-secrets/src/utils/axiosWithAuth.js
+++ family-secrets/src/utils/axiosWithAuth.js
@@ -1,12 +1,10 @@
 const { default: Axios } = require("axios");
 
 export const axiosWithAuth = () => {
-    // const token = localStorage.getItem('token'); //will uncomment when we have backend server
-    const token = 'QpwL5tke4Pnpja7X4';
+    const token = localStorage.getItem('token');
 
-    //baseURL until we can use backend
     return Axios.create({
-        baseURL: 'https://reqres.in/api', 
+        baseURL: '', 
         headers: {
             Authorization: token,
         }
