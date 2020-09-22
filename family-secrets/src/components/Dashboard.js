ac9516da618648d2270919779fce02a4c5dc9593
brittanymclaughlin <britt@brittanytroutman.com>
2020-09-21 22:16:14 -0400

    Some changes to styling regarding the dashboard

--- family-secrets/src/components/Dashboard.js
+++ family-secrets/src/components/Dashboard.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import RecipeCard from './RecipeCard';
 import axios from 'axios';
+import {axiosWithAuth} from '../utils/axiosWithAuth';
+import styled from 'styled-components';
 
 // super crude dashboard for listing recipes
 // current 'props' is canned data stored in app
@@ -15,25 +17,128 @@ import axios from 'axios';
 
 const Dashboard = props => {
   // may require change here
-  const data = props.props;
+  useEffect(() => {
+    axiosWithAuth()
+        .get("api/recipes")
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => console.log(err));
+}, []);
 
+  const data = [
+    {
+      title: 'Chicken Parm',
+      source: 'Nonna',
+      ingredients: '2 lbs chicken, lots of mozz, jar of sauce',
+      instructions: 'cut chicken into small pieces, bread, fry, broil',
+      breakfast: false,
+      lunch: true,
+      dinner: true,
+      dessert: false,
+      vegetarian: false,
+      id: 1
+    },
+    {
+      title: 'Cereal',
+      source: 'Mikey',
+      ingredients: 'Life',
+      instructions: 'Open box, pour in bowl, add milk',
+      breakfast: true,
+      lunch: false,
+      dinner: false,
+      dessert: false,
+      vegetarian: false,
+      id: 2
+    },
+    {
+      title: 'Ommie',
+      source: 'Mommy',
+      ingredients: 'eggs, cheese, bacon, moar cheese',
+      instructions: 'break eggs, throw stuff in',
+      breakfast: true,
+      lunch: true,
+      dinner: true,
+      dessert: false,
+      vegetarian: false,
+      id: 3
+    },
+  ];
+  
 return (
-  <div className="recipe-list">
+  <Dash className="recipe-list">
     <h1>Recipe List</h1>
+    <div className="sections">
     {data.map(recipe => (
       <div className="recipe" key={recipe.id}>
-        <Link to={`/recipe/${recipe.id}`}>
+        <Link to={`/recipe/${recipe.id}`} className="Link">
              <h2>{recipe.title}</h2>
-        <h3>{recipe.source}</h3>
-       
-      </Link>
+             <h3>By: {recipe.source}</h3>
+        </Link>
+        <br />
+        <br />
+        <span className="highlighted">Ingredients You'll Need  </span>
         
-
+        <p>{recipe.ingredients}</p>
       </div>
-    ))}
-  </div>
+    ))}</div>
+  </Dash>
 );
 };
 
+const Dash = styled.div`
+    text-align:center;
+    h1{
+      font-family: 'Satisfy', cursive;
+      font-size:3rem;
+      font-weight:lighter;
+    }
+    .sections{
+    display:flex;
+    flex-wrap:wrap;
+    justify-content:space-evenly;
+    text-align:left;
 
+    
+    .recipe{
+      width:25vw;
+      height:25vw;
+      border-radius:2px;
+      margin:2%;
+      padding:2%;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+      
+      
+    .Link{
+        text-decoration:none;
+
+        h2{
+            color:black;
+            text-decoration:none;
+            font-size:1.5rem;
+            text-align:center;
+            font-weight:lighter;
+            margin-bottom:0px;
+            padding-bottom:0px;
+          }
+          h3{
+            margin-top:0px;
+            text-align:right;
+            margin-right:20%;
+            color:black;
+          }
+      }
+      .highlighted{
+        background-color:orange;
+        font-weight:bolder;
+        font-size:0.8rem;
+        border-radius:2px;
+        color:white;
+        padding:1.5%;
+        width:100%;
+      }
+    
+    }
+  }
+`
 export default Dashboard;
--- family-secrets/src/components/Footer.js
+++ family-secrets/src/components/Footer.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 function Footer () {
 
     return(
         <FooterDiv>
-            <div className="footerlogo">&copy; Secret Family Recipe Cookbook</div>
-           
+            <Link to="/" className="LinkBottom"><span className="logoBottom">Secret Family Secrets Cookbook</span></Link>
+            <div className="links">
+                <Link to="/login">Login</Link>
+                <Link to="/recipe">Add A Recipe</Link>
+                <a href="https://htmlpreview.github.io/?https://github.com/BW-Secret-Family-Recipes-pt/Marketing-Page/blob/marketing/aboutblog/about.html" alt="About">About</a>
+                <a href="https://htmlpreview.github.io/?https://github.com/BW-Secret-Family-Recipes-pt/Marketing-Page/blob/marketing/aboutblog/blog.html" alt="Check out our blog!">Blog</a>
+            </div>
+            <div className="copy">&copy; 2020 Secret Family Recipe Cookbook</div>
+            
         </FooterDiv>
     )
 };
@@ -14,8 +22,33 @@ const FooterDiv = styled.div`
     background-color:#4a3f35;
     height:15vh;
     color:white;
-    font-size:1.0rem;
+    font-size:0.5rem;
     text-align:center;
     padding:4%;
+
+    .links{
+                       
+        display:flex;
+        justify-content:space-between;
+    }
+    .LinkBottom{
+        text-decoration:none;
+        color:white;
+
+        &:hover{
+            color:orange;
+        }
+        
+        .logoBottom{
+            font-family: 'Satisfy', cursive;
+            font-size:1.5rem;
+            font-weight:lighter;
+
+            
+
+        }
+
+    }
+    
 `
 export default Footer;
